08bbd399e40424ac6121e14524f547c77bce3fe3
ekdvs <ekdvsampath02@gmail.com>
2026-04-16 10:18:05 +0530

    Add package.json for user-service-api with express dependency with docker

---
+++ new_Learn/user-service-api/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json(
+    { 
+        message: 'Hello World!' ,
+        name: 'John Doe',
+        age: 30,
+        email: 'ekd@example.com'
+    }
+)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
